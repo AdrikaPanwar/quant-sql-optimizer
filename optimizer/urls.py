a9491446e2062dc510dab151d84e0ea800b6a7c4
Adrika Panwar <162022172+AdrikaPanwar@users.noreply.github.com>
2024-09-29 03:16:24 +0530

    Update urls.py

--- optimizer/urls.py
+++ optimizer/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Home page
+    path('', views.home, name='home'), 
     path('upload/', views.upload_file, name='upload_file'),
     path('result/<int:sql_file_id>/', views.result, name='result'),
 ]
